5a61cbd7cd649ca4b8cb3a33901248a0b0d845c6
Kenneth Obsequio <kenbusano2003@gmail.com>
2022-03-25 19:52:00 +0800

    favicon improving...

--- pages/_document.js
+++ pages/_document.js
@@ -8,10 +8,10 @@ export default function Document() {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;800;900&display=swap" 
             rel="stylesheet" 
         />
-        <link rel="shortcut icon" href="/favicon/favicon.ico"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="shortcut icon" href="/icon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body>
