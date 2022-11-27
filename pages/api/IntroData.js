14953a4ed271cb8b4e41ba12f300cfc653a8b488
lzynth0506 <kenbusano2003@gmail.com>
2022-11-27 09:03:53 +0800

    grammar fix

--- pages/api/IntroData.js
+++ pages/api/IntroData.js
@@ -1,24 +1,22 @@
 //Section
 
-const Quote = [
+export const Quote = [
     {   
         id: "000001",
-        firstMsg: `I'm Kenneth Obsequio, currently a Grade 12 student at San Francisco High School
-        with the TVL-ICT track from Senior High. I've been studying this website development
-        field for 3 years now, wayback to 2017 when someone told me to explore everything 
-        about digital world so my Grade 8 TLE teacher advised us on where to go. She demonstrates 
-        for how to make a decision to choose a typical fields that part of the digital world.`
+        firstMsg: `I'm Kenneth Obsequio, currently a freshman student at Quezon City University.
+        I've been studying website development for four years, beginning in 2017.
+        Someone told me to explore everything about the digital world, so my Grade 8 TLE teacher advised
+        us on where we should go. She demonstrates how to make a decision to choose a typical field.
+        that part of the digital world.`
     },
     {
         id: "000002",
-        secondMsg: `After thinking we made a decision to choose our career field including paths
-        so I choose Website Development so that's where my career story begins studying
-        sort of things is very difficult you need a way to understand every concepts from 
-        programming including problem-solving, understanding the fundamentals for your preffered
-        programming language, how to deploy your project and so on; So in that particular scenarios
-        you will know that is very frustrating to do so. On the other hand you should able to
-        understand what is right and what is wrong for studying coding.`
+        secondMsg: `After thinking, we made a decision to choose our career field, including paths.
+        So I choose website development, and thus my professional journey begins:studying
+        Sorting things out is difficult, and you need a method to understand every concept from start to finish.
+        programming, including problem-solving and understanding the fundamentals of your preferred programming language.
+        programming language, how to deploy your project, and so on; So in those particular scenarios
+        You will understand how frustrating this is.On the other hand, you should be able to
+        Understand everything fundamentally and logically.`
     }
 ]
-
-export default Quote;
