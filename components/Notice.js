9cfeb1ce063dcb740bf24651a0fa4b29352d0dc9
lash0000 <kenbusano2003@gmail.com>
2023-08-28 17:02:34 +0800

    wtf

--- components/Notice.js
+++ components/Notice.js
@@ -4,7 +4,7 @@ export function Notice() {
   return (
     <div className="bg-red-500 p-4 text-center text-white hover:underline">
         <Link href="https://www.lashmono.space/">
-            No longer maintained in favor to my new content (I don't really like this).
+            No longer maintained in favor to my new content (I don&apos;t really like this).
         </Link>
     </div>
   );
